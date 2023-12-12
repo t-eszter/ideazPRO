6bebb00791b9df19c1ef04463cd34c9a092d3e20
Eszter Toth <testimo@Eszters-MBP.fritz.box>
2023-12-12 19:42:53 +0100

    port fix

--- ideazApp/settings.py
+++ ideazApp/settings.py
@@ -40,14 +40,16 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOW_HEADERS = "access-control-allow-origin"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://localhost",
-    "http://localhost:5001",
-    "https://ideaz-pro-025620ea91b7.herokuapp.com",
-    "http://0.0.0.0:8000",
-    "http://0.0.0.0:5001",
-    "http://127.0.0.1",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://localhost",
+#     "https://localhost:5001",
+#     "https://ideaz-pro-025620ea91b7.herokuapp.com",
+#     "https://0.0.0.0:8000",
+#     "https://0.0.0.0:5001",
+#     "https://127.0.0.1",
+# ]
 
 # Application definition
 
