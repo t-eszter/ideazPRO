0ff56911453319567094d827f314fd9c2c75a9ba
Eszter Toth <testimo@Eszters-MacBook-Pro.local>
2023-12-23 08:30:17 +0100

    Added slug to IdeasGrups

--- frontend/admin.py
+++ frontend/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import IdeaGroup, Idea, Person
 
-admin.site.register(IdeaGroup)
+class IdeaGroupAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}  # Automatically populate slug from name
+
+admin.site.register(IdeaGroup, IdeaGroupAdmin)
 admin.site.register(Idea)
 admin.site.register(Person)
