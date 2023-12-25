b563dcf026da434de54b61ce439288f3019ce399
Eszter Toth <testimo@Eszters-MacBook-Pro.local>
2023-12-25 14:16:03 +0100

    Home page

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import IdeaGroup
 from .serializers import IdeaGroupSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
