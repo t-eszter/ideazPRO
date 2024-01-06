96b5d753ee17fab3fb51804a3951f726a12144c5
Eszter Toth <testimo@Eszters-MacBook-Pro.local>
2024-01-06 20:04:31 +0100

    models update

--- frontend/models.py
+++ frontend/models.py
@@ -3,7 +3,7 @@ import uuid
 from autoslug import AutoSlugField
 
 class IdeaGroup(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     description = models.TextField(default='')
     STATUS_CHOICES = [
         ('active', 'Active'),
@@ -22,7 +22,7 @@ class Person(models.Model):
     lastName = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     regDate = models.DateTimeField(auto_now_add=True)
-    profilePic = models.ImageField()
+    profilePic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
     def __str__(self):
         return self.firstName + " " + self.lastName
