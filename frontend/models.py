99d0d4e746b83bae3a87a202862f9b2d9bd96845
Eszter Toth <testimo@Eszters-MacBook-Pro.local>
2024-01-16 09:46:17 +0100

    organizations

--- frontend/models.py
+++ frontend/models.py
@@ -5,6 +5,8 @@ from autoslug import AutoSlugField
 class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class IdeaGroup(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
