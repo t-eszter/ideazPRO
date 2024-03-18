241b9ef10ac25678635336bc0f76e4cbfb9bd579
Eszter Toth <testimo@Eszters-MBP.fritz.box>
2024-03-18 18:43:33 +0100

    idea tagging

--- frontend/admin.py
+++ frontend/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .forms import IdeaGroupForm
 
 # Register your models here.
-from .models import IdeaGroup, Idea, Person, Organization, Vote
+from .models import IdeaGroup, Idea, Person, Organization, Vote, Tag
 
 class IdeaGroupAdmin(admin.ModelAdmin):
     form = IdeaGroupForm 
@@ -14,17 +14,25 @@ class IdeaGroupAdmin(admin.ModelAdmin):
     display_organization.short_description = 'Organization'
 
 class IdeaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'person', 'display_organization', 'postedDate')
-    
+    list_display = ('id', 'title', 'description', 'postedDate', 'group', 'person', 'display_tags', 'display_organization')
+
+    def display_tags(self, obj):
+        """Concatenates and displays all tags related to an idea."""
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    display_tags.short_description = 'Tags'
+
     def display_organization(self, obj):
-        # Accessing the organization through the related IdeaGroup
+        """Displays the organization name of the idea's group, if available."""
         if obj.group and obj.group.organization:
             return obj.group.organization.name
         return "No Organization"
     display_organization.short_description = 'Organization'
 
+    search_fields = ('title', 'description', 'person__name', 'group__name', 'tags__name')
+    list_filter = ('postedDate', 'group', 'tags')
+
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'idea', 'vote_type', 'display_idea_title')  # Customize as needed
+    list_display = ('user', 'idea', 'vote_type', 'display_idea_title')
     
     def display_idea_title(self, obj):
         return obj.idea.title
